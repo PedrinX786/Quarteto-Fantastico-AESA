267e373898274d360666e62a03939d1ac3229742
PedrinX786 <pedrorangeltobiakatsuki@gmail.com>
2025-05-09 19:33:03 -0300

    Add files via upload

---
+++ Desafio 2 Talles.js
@@ -0,0 +1,7 @@
+const livro = {
+    titulo: "A sutil arte de ligar o foda-se",
+    autor: "Mark Manson",
+};
+
+console.log(livro.titulo); // Saida: A sutil arte de ligar o foda-se
+console.log(livro.autor); //Saida: Mark Manson
